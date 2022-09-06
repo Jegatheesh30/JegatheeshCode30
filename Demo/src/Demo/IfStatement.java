3b9d050fcaaccb2ec585d715c67a4b2342441466
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-06 09:02:08 +0530

    if statement

---
+++ Demo/src/Demo/IfStatement.java
@@ -0,0 +1,36 @@
+package Demo;
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num=7589;
+		if(num>0)
+		{
+			int r=num%10;
+			System.out.println(r);
+			num=num/10;
+		}
+		if(num>0)
+		{
+			int r=num%10;
+			System.out.println(r);
+			num=num/10;
+		}
+		if(num>0)
+		{
+			int r=num%10;
+			System.out.println(r);
+			num=num/10;
+		}
+		if(num>0)
+		{
+			int r=num%10;
+			System.out.println(r);
+			num=num/10;
+		}
+	}
+	
+
+}
