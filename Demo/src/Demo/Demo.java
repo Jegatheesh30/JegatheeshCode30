71d337248b18f98b1489a54ddabb56b3edc211f5
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-06 08:54:36 +0530

    if statement

--- Demo/src/Demo/Demo.java
+++ Demo/src/Demo/Demo.java
@@ -13,8 +13,23 @@ public class Demo {
 			System.out.println("bing"+"bang");
 		if(n%3==0||n%5==0)
 			System.out.println(n);
-		
-		
-}
-}
-
+	}
+{
+	int n=5;
+	if(n%3==0&&n%5==0)
+		System.out.println("BING BANG");
+		else if(n%3==0)
+			System.out.println("BING");
+			else if(n%5==0)
+				System.out.println("BANG");
+				else 
+					System.out.println(n);
+}}
+					
+					
+					
+					
+					
+					
+					
+					
