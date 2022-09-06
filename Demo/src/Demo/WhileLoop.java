a899539ca7830b632ca4fdf1b191015aeddf59b1
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-06 12:53:48 +0530

    palindrom checking code

--- Demo/src/Demo/WhileLoop.java
+++ Demo/src/Demo/WhileLoop.java
@@ -1,18 +1,25 @@
 package Demo;
 import java.util.Scanner;
 
+
 public class WhileLoop {
 
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
-		System.out.println("enter an long number");
-		long num=in.nextLong();
-		while (num>0)
+		System.out.println("enter an integer");
+		int num=in.nextInt();
+		int reverse =0;
+		while(num>0)
 		{
-			long r=num%10;
-			System.out.println(r);
+			int r=num%10;
+			reverse=(reverse*10)+r;
 			num=num/10;
 		}
-		
+		System.out.print(reverse);
+		if(reverse==num)
+			System.out.println("= palindrom");
+		else
+			System.out.println("= not palindrom");
 	}
+	
 }
