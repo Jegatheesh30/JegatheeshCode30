e12c4efb1f8a164412fdc77ee333acc9c73b6159
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-06 08:51:52 +0530

    If statement

--- Demo/src/Demo/Demo.java
+++ Demo/src/Demo/Demo.java
@@ -1,40 +1,20 @@
 package Demo;
-import java.util.Scanner;
+
 
 public class Demo {
 	public static void main(String[] args) {
+				
+		int n=12;
+		if(n%3==0)
+			System.out.println("bing");
+		if(n%5==0)
+			System.out.println("bang");
+		if(n%3==0&&n%5==0)
+			System.out.println("bing"+"bang");
+		if(n%3==0||n%5==0)
+			System.out.println(n);
 		
-		int row, i, j, space = 1;  
-		System.out.print("Enter the number of rows you want to print: ");  
-		Scanner sc = new Scanner(System.in);  
-		row = sc.nextInt();  
-		space = row - 1;  
-		for (j = 1; j<= row; j++)  
-		{  
-			for (i = 1; i<= space; i++)  
-			{  
-				System.out.print(" ");  
-			}  
-			space--;  
-			for (i = 1; i <= 2 * j - 1; i++)  
-			{  
-				System.out.print("*");  	
-			}  
-			System.out.println("");  
-		}  
-		space = 1;  
-		for (j = 1; j<= row - 1; j++)  
-		{  
-			for (i = 1; i<= space; i++)  
-			{  
-				System.out.print(" ");  
-			}  
-			space++;  
-			for (i = 1; i<= 2 * (row - j) - 1; i++)  
-			{  
-				System.out.print("*");  
-			}  
-			System.out.println("");  
-		}  
+		
 }
 }
+
