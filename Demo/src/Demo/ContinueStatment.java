5c4b0636710b2ab2d7e76b6ead140b8e9da87768
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-09 13:55:33 +0530

    Continue statment

---
+++ Demo/src/Demo/ContinueStatment.java
@@ -0,0 +1,19 @@
+package Demo;
+
+public class ContinueStatment {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=10;i++)
+		{
+			if(i%2!=0) {//in this statement the remainder of 2 is not eqqual to zero 
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+	}	
+	}
+	
+		
+		
