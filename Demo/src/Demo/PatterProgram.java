5eeb4ad89fb69bf6d13d0ca54d275775f2ec2363
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-09 20:35:25 +0530

    Pattern Program

---
+++ Demo/src/Demo/PatterProgram.java
@@ -0,0 +1,44 @@
+package Demo;
+
+public class PatterProgram {
+
+	public static void main(String[] args) {
+		//program for square patter
+		/*int n=5;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+		*/
+	//program for increasing triangle pattern
+		/*int n=5;
+		for(int i=1;i<=n;i++)
+		{
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print(" *");
+		}
+		System.out.println();
+	}
+	}*/
+	}	
+	//prgram for decreasing triangle pattern
+	{
+	int n=5;
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			System.out.println(" *");
+		}
+		System.out.println();
+	}
+}}
+	
+	
+		
+		
