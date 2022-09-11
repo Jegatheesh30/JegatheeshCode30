8e82e25140a47c97c5535d4fde8c3510bbbe6428
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-11 06:36:49 +0530

    Printing Odd Even

---
+++ Assignment2/src/Assignment/PrintingOddEven.java
@@ -0,0 +1,26 @@
+package Assignment;
+
+public class PrintingOddEven {
+
+	public static void main(String[] args) {
+		
+	int i=0;
+	int j=1;
+	do
+	{
+	if(i%2!=0)
+		System.out.print(i);
+		i++;
+	}
+	while(i<=10);
+	do
+	{
+	if(j%2==0)
+		System.out.print(j);
+		j++;
+	}
+	while(j<=10);
+	}
+}
+
+		
