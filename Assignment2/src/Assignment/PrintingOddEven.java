f6f353c40f285cfd77c7cae671576d0ea6e6b6cb
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-11 09:11:53 +0530

    Printing Odd or event same line

--- Assignment2/src/Assignment/PrintingOddEven.java
+++ Assignment2/src/Assignment/PrintingOddEven.java
@@ -3,24 +3,21 @@ package Assignment;
 public class PrintingOddEven {
 
 	public static void main(String[] args) {
-		
-	int i=0;
-	int j=1;
-	do
-	{
-	if(i%2!=0)
-		System.out.print(i);
-		i++;
+		int i=0;
+		int j=1;
+		do
+		{
+		if(i%2!=0)
+			System.out.print(i);
+			i++;
+		}
+		while(i<=10);
+		do
+		{
+		if(j%2==0)
+			System.out.print(j);
+			j++;
+		}
+		while(j<=10);
+		}
 	}
-	while(i<=10);
-	do
-	{
-	if(j%2==0)
-		System.out.print(j);
-		j++;
-	}
-	while(j<=10);
-	}
-}
-
-		
