a9a0cc52a20db2cacfc58cef3baf21c331e3b39f
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-12 21:25:13 +0530

    Pattern Multiply for additional value

---
+++ Assignment2/src/Assignment/PatternMultiply.java
@@ -0,0 +1,17 @@
+package Assignment;
+
+public class PatternMultiply {
+
+	public static void main(String[] args) {
+		int i,j,a=1;
+		for(i=1;i<=6;i++) {
+			a=1;
+		
+			for(j=1;j<=i;j++) {
+				a=a*i;
+			}
+				System.out.println(a);
+			}
+			}
+		}
+
