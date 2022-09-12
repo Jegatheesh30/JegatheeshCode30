84e01bdddf55d4d7246bbe882a0b4f088227995d
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-12 20:20:04 +0530

    Printing Square Values with loop

--- Assignment2/src/Assignment/PrintingSquareValue.java
+++ Assignment2/src/Assignment/PrintingSquareValue.java
@@ -1,12 +1,12 @@
 package Assignment;
 
-public class PrintingCubeValue {
+public class PrintingSquareValue {
 
 	public static void main(String[] args) {
 		int i=1;
 		for(i=1;i<=10;i++)
 		{
-			int sum=i*i*i;
+			int sum=i*i;
 			System.out.println(sum);
 			
 		}	
