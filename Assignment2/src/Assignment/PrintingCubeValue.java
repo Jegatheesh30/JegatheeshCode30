d79d8e1b6f1dc337dcf578545a69c23577eb7603
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-12 20:17:24 +0530

    Printing Cube value

---
+++ Assignment2/src/Assignment/PrintingCubeValue.java
@@ -0,0 +1,14 @@
+package Assignment;
+
+public class PrintingCubeValue {
+
+	public static void main(String[] args) {
+		int i=1;
+		for(i=1;i<=10;i++)
+		{
+			int sum=i*i*i;
+			System.out.println(sum);
+			
+		}	
+		}	
+}
