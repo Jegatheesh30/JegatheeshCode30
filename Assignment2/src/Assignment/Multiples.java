78e65ad5bd25643eef0c451c1baa2bc9a928084d
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-13 09:19:39 +0530

    Multiples of 3&5

---
+++ Assignment2/src/Assignment/Multiples.java
@@ -0,0 +1,15 @@
+package Assignment;
+
+public class Multiples {
+
+	public static void main(String[] args) {
+		
+		int i=0;
+		for(i=0;i<100;i++)
+		{
+			if(i%3==0&&i%5==0)
+				System.out.println(i);
+
+		}
+	}
+}
