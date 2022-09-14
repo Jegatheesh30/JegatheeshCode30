eb1267293334ba56cf4119c1ff210c49ab9a8950
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-14 17:33:50 +0530

    check wether the prime or not

---
+++ Assignment2/src/Assignment/PrimeNumber.java
@@ -0,0 +1,23 @@
+package Assignment;
+;
+
+public class PrimeNumber {
+	public static void main(String[] args) {
+	int n=83;
+	int m=2;
+	boolean prime=true;
+	while(m<n)
+	{
+		if(n%m==0)
+			prime=false;
+			m++;
+	}
+	if(prime==true)
+		System.out.println(" is prime Number");
+	else 
+		System.out.println(" is not prime Number");
+}	
+}		
+	
+	
+
---
+++ Demo/src/Demo/PrimeNumber.java
@@ -0,0 +1,16 @@
+package Demo;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int m,n=0,s=0;
+		m=23;
+		
+		if(m==1&&m%m==0)
+			System.out.println("give number is prime");
+		else
+			System.out.println("given number is not prime");
+
+	}
+
+}
