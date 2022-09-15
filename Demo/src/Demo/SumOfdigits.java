ef9183513817266b7322cc9d318310273e18dc85
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-15 10:00:36 +0530

    Sum of digit with loop

--- Demo/src/Demo/SumOfdigits.java
+++ Demo/src/Demo/SumOfdigits.java
@@ -1,29 +1,28 @@
 package Demo;
 
-
 public class SumOfdigits {
-	
-	//count the digit 
+
+	// count the digit
 
 	public static void main(String[] args) {
-		int a=214141242;
-		int j,m,k=0;
-		int d=0;
-		while(a>0)
-		{
-			j=a%10;
-			System.out.println(j);
-			a=a/10;
-			k=k+j;}
+		int a = 7589;
+		int j, m, k = 0;
+		int d = 0;
+		while (a > 0) {
+			j = a % 10;
+			//System.out.println(j);
+			a = a / 10;
+			k = k + j;
 			System.out.println(k);
-			{
-				while(k>0) {
-				m=k%10;
-				System.out.println(m);
-				k=k/10;
-				d=d+m;
+
+			if (k > 10) {
+				m = k % 10;
+				d= k / 10;
+				
+				k=d+m;
+				System.out.println(k);
 			}
-			System.out.println(d);
-		}	
+		}
+		System.out.println(k);
 	}
 }
