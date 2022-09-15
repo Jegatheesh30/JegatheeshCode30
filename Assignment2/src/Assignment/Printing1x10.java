7f1f156766e698a1a6e93c99dc6f7784f38d143d
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-15 13:58:51 +0530

    printing 1*10 2*9 3*8 4*7

---
+++ Assignment2/src/Assignment/Printing1x10.java
@@ -0,0 +1,17 @@
+package Assignment;
+
+public class Printing1x10 {
+
+	public static void main(String[] args) {
+		int a=1;
+		int j=10;
+		int sum;
+		while(a<j) {
+			sum=a*j;
+			System.out.println(sum);
+			a++;
+			j--;
+			
+		}
+     	}
+        }
