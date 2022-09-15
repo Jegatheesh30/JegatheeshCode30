f1ca753609d5d9cf95ebcde06a65fc68577e175c
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-15 13:55:26 +0530

    Factorial for given number

---
+++ Assignment2/src/Assignment/Factorial.java
@@ -0,0 +1,20 @@
+package Assignment;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+	/**/
+		
+		int a=5;
+		int fact=1;
+		int i=1;
+		while(i<=a) {
+			fact=fact*i;
+			i++;
+		}
+		System.out.println(fact);
+		
+			}
+	}
+	
+
