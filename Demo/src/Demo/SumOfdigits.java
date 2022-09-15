fa33c0876a8e7bcee3021530e566f7d1bd83df1f
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-15 09:30:05 +0530

    Sum of digit with nested loop

--- Demo/src/Demo/SumOfdigits.java
+++ Demo/src/Demo/SumOfdigits.java
@@ -1,21 +1,29 @@
 package Demo;
 
+
 public class SumOfdigits {
 	
 	//count the digit 
 
 	public static void main(String[] args) {
-		int a=122342994;
-		int b,c,d=0;
-		int j=0;
-		for(;a>0;) {
-		b=a%10;
-		System.out.print(b+ "+");
-		a=a/10;
-		//a=c;
-		d=d+b;
-		}
-		System.out.println(d);
+		int a=214141242;
+		int j,m,k=0;
+		int d=0;
+		while(a>0)
+		{
+			j=a%10;
+			System.out.println(j);
+			a=a/10;
+			k=k+j;}
+			System.out.println(k);
+			{
+				while(k>0) {
+				m=k%10;
+				System.out.println(m);
+				k=k/10;
+				d=d+m;
+			}
+			System.out.println(d);
+		}	
 	}
-	}
-
+}
