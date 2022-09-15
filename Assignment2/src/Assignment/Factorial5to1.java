f06f99fd43978c4a8d4e88eb6aed6dc9a2ab34b9
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-15 13:57:09 +0530

    Factorial of number 5 to 1

---
+++ Assignment2/src/Assignment/Factorial5to1.java
@@ -0,0 +1,17 @@
+package Assignment;
+
+public class Factorial5to1 {
+
+	public static void main(String[] args) {
+		int a=5;
+		int i;
+		for(i=5;i>=1;i--) {
+			a=1;
+			for(int j=1;j<=i;j++) {
+				a=a*j;
+				
+			}
+			System.out.println(a);
+		}
+	}
+    }
