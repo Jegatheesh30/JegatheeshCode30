bd910bf63b30516ff0e502a06020322fcba7927f
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-22 09:47:03 +0530

    diamond hollow patter

--- Demo/src/Demo/Pattern12.java
+++ Demo/src/Demo/Pattern12.java
@@ -3,16 +3,31 @@ package Demo;
 public class Pattern12 {
 
 	public static void main(String[] args) {
-		int n=5;
-		for(int i=1;i<n;i++)
-		{
-			for(int j=1;j<=i;j++) {
-				if(i%2==0)
-					System.out.print(" 2");
+		int n = 9;
+		for (int i = 1; i <= n; i++) {
+			for (int j = i; j <= n; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=i; j++) {
+				if (j == 1 || j == i)
+					System.out.print(" *");
 				else
-					System.out.print(" 1");
-				
-			}System.out.println();
+					System.out.print("  ");
+			}
+			System.out.println();
+		}
+		n = 9;
+		for (int i = 1; i <=n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = i; j <= n; j++) {
+				if (j == 1 || j == n || j == i)
+					System.out.print(" *");
+				else
+					System.out.print("  ");
+			}
+			System.out.println();
 		}
 	}
 }
