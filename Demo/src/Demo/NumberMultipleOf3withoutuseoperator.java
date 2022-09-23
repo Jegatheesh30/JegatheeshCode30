a7f96d1c57d3530114fd0fb7016d74c629dc29b2
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-23 11:52:02 +0530

    Number is multiple of 3 program without using / and % operator.

---
+++ Demo/src/Demo/NumberMultipleOf3withoutuseoperator.java
@@ -0,0 +1,17 @@
+package Demo;
+
+public class NumberMultipleOf3withoutuseoperator {
+
+	public static void main(String[] args) {
+		int n=2;
+		int a=0,b=0;
+			a=1;
+			for(int j=0;j<100;j+=3) {
+				b=a*3;
+				a++;
+				System.out.println(j);
+			}
+		}
+	}
+
+
