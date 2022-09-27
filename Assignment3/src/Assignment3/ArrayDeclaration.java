327008348f595ef76f356b54a1b3169a05d93536
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-27 15:13:09 +0530

    Array Declaration

---
+++ Assignment3/src/Assignment3/ArrayDeclaration.java
@@ -0,0 +1,19 @@
+package Assignment3;
+
+public class ArrayDeclaration {
+
+	public static void main(String[] args) {
+		int a[]=new int[5];//size of an Array
+		a[0]=1;
+		a[1]=2;
+		a[2]=3;
+		a[3]=4;
+		a[4]=5;
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+		System.out.println(a[4]);
+	}
+
+}
