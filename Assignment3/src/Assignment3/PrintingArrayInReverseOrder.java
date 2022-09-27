11b479f23a9bad2c642cbf8e24cd47dfa4b34cf5
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-27 16:21:20 +0530

    Printing Array in reverse Order

---
+++ Assignment3/src/Assignment3/PrintingArrayInReverseOrder.java
@@ -0,0 +1,12 @@
+package Assignment3;
+
+public class PrintingArrayInReverseOrder {
+
+	public static void main(String[] args) {
+		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int i=9;
+		for(i=a.length-1;i>=0;i--) {
+			System.out.println(a[i]);
+	}
+	}
+}
