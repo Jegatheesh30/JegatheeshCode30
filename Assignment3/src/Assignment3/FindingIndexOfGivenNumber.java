5f32e0aa5d92bda0a3c2fefc939adbe8aacbdb0d
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-27 17:06:15 +0530

    Find given number in array

---
+++ Assignment3/src/Assignment3/FindingIndexOfGivenNumber.java
@@ -0,0 +1,14 @@
+package Assignment3;
+
+public class FindingIndexOfGivenNumber {
+
+	public static void main(String[] args) {
+		int array[] = { 1, 2, 3, 234, 2, 323, 5, 345, 345, 23, 43, 42, 4 };
+		int x = 234;
+		for (int i = 0; i < array.length; i++) {
+			if (x == array[i])
+				System.out.println("The index of given Number in Array =" + i);
+
+		}
+	}
+}
