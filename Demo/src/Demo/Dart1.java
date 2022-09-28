75a53c389097e549b3976fca381d4aab96cd1c6e
jegatheesh <jegatheesh@jegatheesh-Lenovo-E41-15>
2022-09-28 13:20:29 +0530

    ATM amount distribution program Rs.500/- , RS.200/- , Rs.100/-

--- Demo/src/Demo/Dart1.java
+++ Demo/src/Demo/Dart1.java
@@ -12,14 +12,14 @@ public class Dart1 {
 		int b=200;
 		int c=100;
 		int m=n/a;
-		System.out.println(m);
+		System.out.println("Number Of 500 x "+m);
 		int k=m*a;
 		int o=n-k;
 		int l=o/b;
 		int i=o%b;
-		System.out.println(l);
+		System.out.println("Number Of 200 x "+l);
 		int j=i/c;
-		System.out.println(j);
+		System.out.println("Number Of 100 x "+j);
 		}
 		else 
 			System.out.println("ivalid amount enter above 500");
