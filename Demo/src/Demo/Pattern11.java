408abacbbd676d43a41f78ab9f0b6d112f56cec3
jegatheesh <user.email>
2022-10-11 09:58:49 +0530

    Hollow Number Patter update

--- Demo/src/Demo/Pattern11.java
+++ Demo/src/Demo/Pattern11.java
@@ -3,23 +3,55 @@ package Demo;
 public class Pattern11 {
 
 	public static void main(String[] args) {
-		int a = 5;
-		int b;
-		for (int i = 0; i <= a; i++) {
-			for (int j = 0; j <= a; j++) {
-				if (i == 0 || i == 5) {
-					b = i < a / 2 ? j : a - j;
-					System.out.print(b + " ");
-				} else if (j == 0 || j == 5) {
-					b = j < a / 2 ? i : a - i;
-					System.out.print(b + " ");
-				} else {
+		int n=9;
+		int i=1;
+		int j=1;
+		
+		for(i=1;i<=n;i++) {
+			for(j=1;j<=n;j++) {
+				if(i==1)
+					System.out.print(j+" ");
+				else if(i==n)
+					System.out.print(i-j+1+" ");
+				else if(j==1)
+					System.out.print(i+" ");
+				else if(j==n)
+					System.out.print(j-i+1+"  ");
+				else
 					System.out.print("  ");
-				}
-			}
-			System.out.println();
+			}System.out.println();
+			
 		}
-
 	}
-
-}
+}	
+		
+		
+/*		int n=4;
+		int i=0;
+		int j=1;
+		for(i=1;i<=n;i++) {
+			System.out.print(i+" ");
+			j++;
+		}
+		System.out.println();
+		j=1;
+		for(i=1;i<=n;i++) {
+			if(i==1||i==n)
+			System.out.print(++j+" ");
+			else
+				System.out.print("  ");
+		}
+		System.out.println();
+		j=4;
+		for(i=1;i<=n;i++) {
+			if(i==1||i==n)
+				System.out.print(--j+" ");
+			else
+				System.out.print("  ");
+		}
+		System.out.println();
+		j=1;
+		for(i=4;i>=j;i--) {
+			System.out.print(i+" ");
+		}
+	*/	
