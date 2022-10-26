d5b4ca24b568325183a4319a04df870d63c30e9b
Jegatheesh30 <jegatheesh3096@gmail.com>
2022-10-26 15:59:50 +0530

    constructor practice

---
+++ practice/src/practice/calc.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class calc {
+	int num1;
+	int num2;
+	int result;
+	
+	public calc() {
+		num1=23;
+		num2=45;
+		result=num1+num2;
+		System.out.println("the answer is =" +result);
+	}
+
+	public static void main(String[] args) {
+		calc obj=new calc();
+		
+}
+
+}
