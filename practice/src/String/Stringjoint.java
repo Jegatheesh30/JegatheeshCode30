7d482805a62fd21a5e6d27a3e943cd63f6e37eac
Jegatheesh30 <jegatheesh3096@gmail.com>
2022-10-27 08:39:10 +0530

    Concatination of the string in java

---
+++ practice/src/String/Stringjoint.java
@@ -0,0 +1,13 @@
+package String;
+
+public class Stringjoint {
+	public static void main(String[] args) {
+		String first="hellow";
+		System.out.println("fist string :"+ first);
+		String second="world!";
+		System.out.println("second string :"+ second);
+		String joinedString=first.concat(second);
+		System.out.println("After concatination :"+joinedString);
+	}
+
+}
