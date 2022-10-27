d7ec55dc8a97c3a85c87e5670903a6ea2dcf5c33
Jegatheesh30 <jegatheesh3096@gmail.com>
2022-10-27 07:59:51 +0530

    find the length of the string

---
+++ practice/src/String/Stringlength.java
@@ -0,0 +1,14 @@
+package String;
+
+public class Stringlength {
+
+	public static void main(String[] args) {
+		String greet="hellow world!";
+		System.out.println("String : "+ greet);
+	
+	
+		int length = greet.length();
+		System.out.println("length of the string : "+ length);
+	
+	}
+}
