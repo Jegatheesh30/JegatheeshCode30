adcced0e175f6e1d6efa782e560534bd7faf39d5
Jegatheesh30 <jegatheesh3096@gmail.com>
2022-11-14 13:04:41 +0530

    Mulitiplying one dimensional array with various lenth

--- Array/src/Namepreview.java
+++ Array/src/Namepreview.java
@@ -2,21 +2,29 @@
 public class Namepreview {
 
 	public static void main(String[] args) {
-		int a[]= {10,20,30};
-		int b[]= {40,50};
-		int k=0;
-		int len=a.length+b.length;
-		int c[]=new int[len];
-		for(int i=0;i<a.length;i++) {
-			c[i]=a[i];
-			
+		int a[] = { 1,2,3,4,5 };
+		int b[] = { 1,2,5,5,2,2,2,2 };
+
+		int len1 = a.length;
+		int len2 = b.length;
+
+		int small = len1 < len2 ? len1 : len2;
+		int big = len1 > len2 ? len1 : len2;
+		int c[] = new int[big];
+		for (int i = 0; i < small; i++) {
+			c[i] = a[i] * b[i];
 		}
-		for(int i=a.length;i<c.length;i++) {
-			c[i]=b[k];
-			k++;
+		if (a.length > b.length) {
+			for (int i = small; i < c.length; i++) {
+				c[i] = a[i];
+			}
+		} else {
+			for (int i = small; i < c.length; i++) {
+				c[i] = b[i];
+			}
 		}
-		for(int i=0;i<c.length;i++) {
+		for (int i = 0; i < c.length; i++) {
 			System.out.println(c[i]);
 		}
-		}
+	} 
 }
